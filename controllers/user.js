6de0ca290bc75f1c555474479cdfc0fbc17ed5b7
Swastik <swastikkumar50@gmail.com>
2021-11-11 15:32:22 +0530

    Initialized Controllers

---
+++ controllers/user.js
@@ -0,0 +1,31 @@
+const asyncHandler = require('../middlewares/async')
+const errorHandler = require('../utils/errorHandler')
+const User = require('../models/User')
+
+exports.getUsers = asyncHandler(async (req, res, next) => {
+    const user = await User.find()
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+})
+
+exports.getUser = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.params.id)
+
+    if (!user) {
+        return next(new errorHandler(`User Not Found`, 404))
+    }
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+})
+
+exports.createUser = asyncHandler(async (req, res, next) => {
+    const user = await User.create(req.body)
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+})
